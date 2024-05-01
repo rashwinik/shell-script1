4e890ce84b068ab87c557d7c913d390a117260ee
rashwinik <rashwinik5566@gmail.com>
2024-05-01 18:08:56 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,12 +2,11 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo " please run this script with root access."
     exit 1 # manually exit if error comes.
 else
-
     echo " you are super user. "
 fi    
 
